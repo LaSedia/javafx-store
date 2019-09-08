b2cc3b1c3fe180711d129a4ff4dfe1f1771edb84
LaSedia <leonardoffline@gmail.com>
2019-09-08 19:29:02 -0300

    loadView

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -3,6 +3,7 @@ package gui;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.function.Consumer;
 
 import application.Main;
 import gui.util.Alerts;
@@ -36,16 +37,19 @@ public class MainViewController implements Initializable	{
 	@FXML
 	public void onMenuItemDepartmentAction()	{
 		System.out.println("clicou dep");
-		loadView("/gui/DepartmentList.fxml");
+		loadView("/gui/DepartmentList.fxml", (DepartmentListController controller) ->	{
+			controller.setDepartmentService(new DepartmentService());
+			controller.updateTableView();
+		});
 	}
 
 	@FXML
 	public void onMenuItemAboutAction()	{
 		System.out.println("clicou about");
-		loadView("/gui/About.fxml");
+		loadView("/gui/About.fxml", x -> {});
 	}
 	
-	private void loadView(String absolutName)	{
+	private synchronized <T> void loadView(String absolutName, Consumer<T> initializingAction)	{
 		try	{
 		FXMLLoader loader = new FXMLLoader(getClass().getResource(absolutName));
 		VBox newVBox = loader.load();
@@ -59,11 +63,9 @@ public class MainViewController implements Initializable	{
 		mainVBox.getChildren().clear();
 		mainVBox.getChildren().add(mainMenu);
 		mainVBox.getChildren().addAll(newVBox.getChildren());
-		
-		DepartmentListController controller = loader.getController(); //POG APAGAR
-		controller.setDepartmentService(new DepartmentService());
-		controller.updateTableView();
-		
+
+		T controller = loader.getController();
+		initializingAction.accept(controller);
 				
 		} catch(IOException e)	{
 			Alerts.showAlert("IOException", "Erro loading view", e.getMessage(), AlertType.ERROR);
