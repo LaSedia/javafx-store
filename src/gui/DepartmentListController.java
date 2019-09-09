312c77734ba5c7e0e7b255656a40fc7350147f6b
LaSedia <leonardoffline@gmail.com>
2019-09-08 22:39:11 -0300

    DepartmentForm design

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -1,20 +1,29 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 
 import application.Main;
-import model.entities.Department;
+import gui.util.Alerts;
+import gui.util.Utils;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
+import model.entities.Department;
 import model.services.DepartmentService;
 
 public class DepartmentListController implements Initializable{
@@ -34,8 +43,10 @@ public class DepartmentListController implements Initializable{
 	private Button btNew;
 
 	@FXML
-	public void onBtNewAction()	{
-		System.out.println("clicou bt new");
+	public void onBtNewAction(ActionEvent event)	{
+		System.out.println("clicou new");
+		Stage parentStage = Utils.currentStage(event);
+		createDialogForm("/gui/DepartmentForm.fxml", parentStage);
 	}
 
 	@FXML
@@ -66,6 +77,30 @@ public class DepartmentListController implements Initializable{
 
 	public void setDepartmentService(DepartmentService service) {
 		this.service = service;
+	}
+	
+	private void createDialogForm(String absoluteName, Stage parentStage) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			Pane pane = loader.load();
+
+//			DepartmentFormController controller = loader.getController();
+//			controller.setDepartment(obj);
+//			controller.setDepartmentService(new DepartmentService());
+//			controller.subscribeDataChangeListener(this);
+//			controller.updateFormData();
+
+			Stage dialogStage = new Stage();
+			dialogStage.setTitle("Enter Department data");
+			dialogStage.setScene(new Scene(pane));
+			dialogStage.setResizable(false);
+			dialogStage.initOwner(parentStage);
+			dialogStage.initModality(Modality.WINDOW_MODAL);
+			dialogStage.showAndWait();
+		} catch (IOException e) {
+			e.printStackTrace();
+			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
+		}
 	}
 
 }
