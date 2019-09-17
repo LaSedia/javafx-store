7ff9c139ae3eb384b498295ecb53fd5e6df598f7
LaSedia <leonardoffline@gmail.com>
2019-09-17 00:20:31 -0300

    seller form

--- src/gui/SellerFormController.java
+++ src/gui/SellerFormController.java
@@ -1,8 +1,11 @@
 package gui;
 
 import java.net.URL;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.ResourceBundle;
 import java.util.Set;
@@ -17,6 +20,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import model.entities.Seller;
@@ -38,7 +42,25 @@ public class SellerFormController implements Initializable{
 	private TextField txtName;
 	
 	@FXML
+	private TextField txtEmail;
+	
+	@FXML
+	private DatePicker dpBirthDate;
+	
+	@FXML
+	private TextField txtBaseSalary;
+	
+	@FXML
 	private Label lblErrorName;
+	
+	@FXML
+	private Label lblErrorEmail;
+	
+	@FXML
+	private Label lblErrorBirthDate;
+	
+	@FXML
+	private Label lblErrorBaseSalary;
 	
 	@FXML
 	private Button btSave;
@@ -123,15 +145,24 @@ public class SellerFormController implements Initializable{
 	
 	private void initializeNodes()	{
 		Constraints.setTextFieldInteger(txtId);
-		Constraints.setTextFieldMaxLength(txtName, 30);
+		Constraints.setTextFieldMaxLength(txtName, 70);
+		Constraints.setTextFieldDouble(txtBaseSalary);
+		Constraints.setTextFieldMaxLength(txtEmail, 60);
+		Utils.formatDatePicker(dpBirthDate, "dd/MM/yyyy");
 	}
 	
 	protected void updateFormData()	{
 		if (seller == null) {
 			throw new IllegalStateException ("Entity was null!");
 		}
+		Locale.setDefault(Locale.US);
 		txtId.setText(String.valueOf(seller.getId()));
 		txtName.setText(seller.getName());
+		txtEmail.setText(seller.getEmail());
+		txtBaseSalary.setText(String.format("%.2f", seller.getBaseSalary()));
+		if (seller.getBirthDate() != null) {
+			dpBirthDate.setValue(LocalDate.ofInstant(seller.getBirthDate().toInstant(), ZoneId.systemDefault()));
+		}
 	}
 
 }
